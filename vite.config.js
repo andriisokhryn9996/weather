f54857d6caa44d66c922b29d2a493ebc701a9ad3
AndriiSokhryn <andriisokhryn@elephantslab.com>
2023-03-05 13:50:21 +0200

    added "base" to vite config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 dotenv.config()
 export default defineConfig({
+  base: '/weather/',
   plugins: [vue()],
   define: {
     'process.env': process.env
